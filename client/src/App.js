35f2d357fa86210781abe4ba4d98c1d8ecd8535e
imohsinali <mohsinbalty@gmail.com>
2022-09-17 15:24:21 +0500

    set Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <Router>
+      <div className="App">Hello world</div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chats" element={<ChatPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
