552fad5068356683eed145e4d9a4ec8a68ecac1b
imohsinali <mohsinbalty@gmail.com>
2022-09-18 20:40:14 +0500

    Update userModel.js

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
-
-const messageModel = mongoose.Schema(
+import bcrypt from "bcryptjs";
+const userSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     pic: {
       type: String,
-      required: true,
       default:
         "https://media.istockphoto.com/vectors/user-icon-flat-isolated-on-white-background-user-symbol-vector-vector-id1300845620?k=20&m=1300845620&s=612x612&w=0&h=f4XTZDAv7NPuZbG0habSpU0sNgECM0X7nbKzTUta3n8=",
     },
@@ -16,7 +15,17 @@ const messageModel = mongoose.Schema(
     timestamps: true,
   }
 );
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
-const User = mongoose.model("User", messageModel);
+const User = mongoose.model("User", userSchema);
 
 export default User;
