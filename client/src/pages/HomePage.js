85f76070fba7da1e3ce242389d082c01641be2a1
imohsinali <mohsinbalty@gmail.com>
2022-09-18 20:35:40 +0500

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,7 +1,61 @@
 import React from "react";
-
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import Login from "../components/Auth/Login";
+import SignUp from "../components/Auth/SignUp";
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container maxW={"xl"} centerContent>
+      <Box
+        d="flex"
+        textAlign={"center"}
+        justifyContent="center"
+        alignItems="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius={"lg"}
+        borderWidth="1px"
+      >
+        <Text fontSize={"4xl"} fontFamily="work sans">
+          B-Talk
+        </Text>
+      </Box>
+
+      <Box
+        bg={"white"}
+        w="100%"
+        p={4}
+        borderRadius="lg"
+        borderWidth={"1px"}
+        color="black"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb={"1em"}>
+            <Tab w="50%">Login</Tab>
+            <Tab w="50%">Signup</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
