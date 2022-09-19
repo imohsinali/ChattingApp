39e1e1d967024694c6bb2476a98f5b12ec0aa1b5
imohsinali <mohsinbalty@gmail.com>
2022-09-19 20:18:10 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,29 +1,13 @@
 import express from "express";
-import chats from "./data/data.js";
 import * as dotenv from "dotenv";
 import connectDb from "./config/db.js";
-import colors from "colors";
 import userRoutes from "./routes/userRoutes.js";
+import color from "colors";
 import { errorHandle, notFound } from "./middlewares/errorMiddleware.js";
 dotenv.config();
 connectDb();
 const app = express();
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello word");
-});
-
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  //   console.log(req.params.id);
-  const singleChat = chats.find((chat) => chat._id === req.params.id);
-  //   console.warn(singleChat);
-  res.send(singleChat);
-});
 app.use("/api/user", userRoutes);
 app.use(notFound);
 app.use(errorHandle);
