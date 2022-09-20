646eea01271189585afd5b21739aa286603ca4bd
imohsinali <mohsinbalty@gmail.com>
2022-09-20 11:29:31 +0500

    Update userController.js

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -52,3 +52,19 @@ export const authUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid Email or password");
   }
 });
+
+//api/users?search=mohsin this is called query
+export const allUsers = async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          {
+            name: { $regex: req.query.search, $options: "i" },
+            email: { $regex: req.query.search, $options: "i" },
+          },
+        ],
+      }
+    : {};
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+};
