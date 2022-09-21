c5cfcc2cb476c4e2d7069dec945d20d14a3cf3a7
imohsinali <mohsinbalty@gmail.com>
2022-09-21 21:27:01 +0500

    Update authMiddeware.js

--- server/middlewares/authMiddeware.js
+++ server/middlewares/authMiddeware.js
@@ -4,24 +4,22 @@ import asyncHandler from "express-async-handler";
 
 export const protect = asyncHandler(async (req, res, next) => {
   let token;
-  let decode;
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
-      decode = jwt.verify(token, process.env.JWT_S);
-      req.user = await User.findById(decode.id).select("-password");
-      console.log("Mohsin");
+      const decode = jwt.verify(token, "MOHSIN");
+
+      const user = await User.findById(decode.id);
+      req.user = decode;
+      console.log("decode", decode);
+      console.log(user);
+      console.log(token);
+
       next();
     } catch (e) {
-      console.log(
-        jwt.verify(
-          "zeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NjM2MTUwMDEsImV4cCI6MTY2NjIwNzAwMX0.o4NEmOVqmyb9Wb_Bj3U4TcqaKERrd1dOmBohUgG7C8A",
-          process.env.JWT_S
-        )
-      );
       res.status(401);
       throw new Error("Not authorized, token failed");
     }
