a5078fb32c12953e782393a3aae25c7da407edcf
imohsinali <mohsinbalty@gmail.com>
2022-09-21 21:26:46 +0500

    Update generateToke.js

--- server/config/generateToke.js
+++ server/config/generateToke.js
@@ -1,9 +1,11 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_S, {
+  console.log(id);
+  const token = jwt.sign({ id }, process.env.JWT_S, {
     expiresIn: "30d",
   });
-};
+return token
+}
 
 export default generateToken;
