fa318265b1fa16d8b681eb69cdb02e7690610946
Mohsin Ali <59974279+imohsinali@users.noreply.github.com>
2022-10-14 21:41:38 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import chatRoutes from './routes/chatRoutes.js'
 import color from "colors";
 import { errorHandle, notFound } from "./middlewares/errorMiddleware.js";
 dotenv.config();
@@ -9,6 +10,7 @@ connectDb();
 const app = express();
 app.use(express.json());
 app.use("/api/user", userRoutes);
+app.use("/api/chat",chatRoutes)
 app.use(notFound);
 app.use(errorHandle);
 const port = process.env.PORT || 5000;
